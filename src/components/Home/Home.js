27a2a91f2a38a52cb15bdc7c3dd4379e88232091
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-05 23:58:38 +0300

    Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+export function Header() {
+    return (
+        <>
+            {/* Home page */}
+            <section id="home">
+                <img src="./images/landing.png" alt="home" />
+                <h2 id="landing-text">
+                    <span>Add your favourite albums</span> <strong>||</strong>{" "}
+                    <span>Discover new ones right here!</span>
+                </h2>
+            </section>
+        </>
+    );
+}
