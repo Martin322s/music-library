a2a0fa353b48046c6769df5625863120f76ed475
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-07 13:50:35 +0300

    login controlled component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,37 @@
 import { useState } from "react";
+import { changeHandler } from "../../utils/utils";
 
 export function Login() {
     const [data, setData] = useState({
         email: "",
         password: ""
     });
-    
+
+    const submitHandler = (ev, userData) => {
+        ev.preventDefault();
+        console.log(userData);
+    }
+
     return (
         <section id="login">
             <div className="form">
                 <h2>Login</h2>
-                <form className="login-form">
-                    <input type="text" name="email" id="email" placeholder="email" />
+                <form className="login-form" onSubmit={(ev) => submitHandler(ev, data)}>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="email"
+                        value={data.email}
+                        onChange={(ev) => changeHandler(ev, setData)}
+                    />
                     <input
                         type="password"
                         name="password"
                         id="password"
                         placeholder="password"
+                        value={data.password}
+                        onChange={(ev) => changeHandler(ev, setData)}
                     />
                     <button type="submit">login</button>
                     <p className="message">
