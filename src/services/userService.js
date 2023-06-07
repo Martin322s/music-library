b6743bbe4210a6c1bbf0bd5a900439e2c51f2c16
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-07 11:52:16 +0300

    register service call

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,13 @@
 const baseUrl = "http://localhost:3030/users";
+
+export const registerUser = async (userData) => {
+    const res = await fetch(`${baseUrl}/register`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    });
+    
+    return res.json();
+}
