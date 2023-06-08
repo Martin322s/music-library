df676ad39530f92c40bddd039e82fea6910f0ffe
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-08 19:40:48 +0300

    logout user from the server

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,12 +1,14 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
+import { logoutUser } from "../../services/userService";
 
 export function Logout() {
     const { userLogout } = useContext(AuthContext);
 
     useEffect(() => {
         userLogout();
+        logoutUser();
     }, [userLogout]);
     return <Navigate to="/" replace />
 }
