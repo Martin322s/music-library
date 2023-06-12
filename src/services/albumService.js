3f6ab90fdf48acc171399716fa21b9f3f9ba88d9
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-12 22:26:28 +0300

    create album service

--- src/services/albumService.js
+++ src/services/albumService.js
@@ -2,3 +2,16 @@ const baseUrl = "http://localhost:3030/data";
 
 export const getAllAlbums = () => fetch(`${baseUrl}/albums?sortBy=_createdOn%20desc`).then(res => res.json());
 export const getOneAlbum = (albumId) => fetch(`${baseUrl}/albums/${albumId}`).then(res => res.json());
+
+export const createAlbum = async (token, albumData) => {
+    const res = await fetch(`${baseUrl}/ablums`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(albumData)
+    });
+    const result = await res.json();
+    return result;    
+}
