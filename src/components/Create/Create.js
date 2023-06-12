fb7cb129fb278d5ae4c49cbffd8b7d589f98b691
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-12 22:47:04 +0300

    controlled create component

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,36 +1,88 @@
+import { useState } from "react";
+
 export function Create() {
+    const [data, setData] = useState({
+        singer: "",
+        album: "",
+        imageUrl: "",
+        release: "",
+        label: "",
+        sales: ""
+    });
+
+    const changeHandler = (ev) => {
+        setData(state => ({
+            ...state,
+            [ev.target.name]: ev.target.value
+        }));
+    }
+
+    const submitHandler = (ev, albumData) => {
+        ev.preventDefault();
+        
+        if (!Object.values(albumData).some(x => x === "")) {
+
+        } else {
+            alert("All fields are required!");
+        }
+    }
+
     return (
-        <>
-            {/* Create Page (Only for logged-in users) */}
-            <section id="create">
-                <div className="form">
-                    <h2>Add Album</h2>
-                    <form className="create-form">
-                        <input
-                            type="text"
-                            name="singer"
-                            id="album-singer"
-                            placeholder="Singer/Band"
-                        />
-                        <input type="text" name="album" id="album-album" placeholder="Album" />
-                        <input
-                            type="text"
-                            name="imageUrl"
-                            id="album-img"
-                            placeholder="Image url"
-                        />
-                        <input
-                            type="text"
-                            name="release"
-                            id="album-release"
-                            placeholder="Release date"
-                        />
-                        <input type="text" name="label" id="album-label" placeholder="Label" />
-                        <input type="text" name="sales" id="album-sales" placeholder="Sales" />
-                        <button type="submit">post</button>
-                    </form>
-                </div>
-            </section>
-        </>
+        <section id="create">
+            <div className="form">
+                <h2>Add Album</h2>
+                <form className="create-form" onSubmit={(ev) => submitHandler(ev, data)}>
+                    <input
+                        type="text"
+                        name="singer"
+                        id="album-singer"
+                        placeholder="Singer/Band"
+                        value={data.singer}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input 
+                        type="text" 
+                        name="album" 
+                        id="album-album" 
+                        placeholder="Album"
+                        value={data.album}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        id="album-img"
+                        placeholder="Image url"
+                        value={data.imageUrl}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input
+                        type="text"
+                        name="release"
+                        id="album-release"
+                        placeholder="Release date"
+                        value={data.release}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input 
+                        type="text" 
+                        name="label" 
+                        id="album-label" 
+                        placeholder="Label"
+                        value={data.label}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <input 
+                        type="text" 
+                        name="sales" 
+                        id="album-sales" 
+                        placeholder="Sales"
+                        value={data.sales}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <button type="submit">post</button>
+                </form>
+            </div>
+        </section>
     );
 }
