194f657d796d216230eb97a87519da5a39409324
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-13 21:09:12 +0300

    controlled edit component

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,36 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Edit() {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        singer: "",
+        album: "",
+        imageUrl: "",
+        release: "",
+        label: "",
+        sales: ""
+    });
+
+    const changeHandler = (ev) => {
+        setData(state => ({
+            ...state,
+            [ev.target.name]: ev.target.value
+        }));
+    }
+
     return (
-        <>
-            {/* Edit Page (Only for logged-in users) */}
-            <section id="edit">
-                <div className="form">
-                    <h2>Edit Album</h2>
-                    <form className="edit-form">
-                        <input
-                            type="text"
-                            name="singer"
-                            id="album-singer"
-                            placeholder="Singer/Band"
-                        />
-                        <input type="text" name="album" id="album-album" placeholder="Album" />
-                        <input
-                            type="text"
-                            name="imageUrl"
-                            id="album-img"
-                            placeholder="Image url"
-                        />
-                        <input
-                            type="text"
-                            name="release"
-                            id="album-release"
-                            placeholder="Release date"
-                        />
-                        <input type="text" name="label" id="album-label" placeholder="Label" />
-                        <input type="text" name="sales" id="album-sales" placeholder="Sales" />
-                        <button type="submit">post</button>
-                    </form>
-                </div>
-            </section>
-        </>
+        <section id="edit">
+            <div className="form">
+                <h2>Edit Album</h2>
+                <form className="edit-form">
+                    <input
+                        type="text"
+                        name="singer"
+                        id="album-singer"
+                        placeholder="Singer/Band"
+                        value={data.singer}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input 
+                        type="text" 
+                        name="album" 
+                        id="album-album" 
+                        placeholder="Album"
+                        value={data.album}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        id="album-img"
+                        placeholder="Image url"
+                        value={data.imageUrl}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input
+                        type="text"
+                        name="release"
+                        id="album-release"
+                        placeholder="Release date"
+                        value={data.release}
+                        onChange={(ev) => changeHandler(ev)}
+                    />
+                    <input 
+                        type="text" 
+                        name="label" 
+                        id="album-label" 
+                        placeholder="Label"
+                        value={data.label}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <input 
+                        type="text" 
+                        name="sales" 
+                        id="album-sales" 
+                        placeholder="Sales"
+                        value={data.sales}
+                        onChange={(ev) => changeHandler(ev)} 
+                    />
+                    <button type="submit">post</button>
+                </form>
+            </div>
+        </section>
     );
 }
