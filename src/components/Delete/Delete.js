6edcf28c5b9ef1c0cfa712f0c908e923d09245af
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-13 21:30:00 +0300

    delete album

--- src/components/Delete/Delete.js
+++ src/components/Delete/Delete.js
@@ -1,5 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import * as albumService from "../../services/albumService";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export function Delete() {
     const { albumId } = useParams();
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        albumService.deleteAlbum(albumId, user.accessToken)
+            .then(() => navigate("/catalog"));
+    }, [albumId, navigate, user.accessToken]);
 }
